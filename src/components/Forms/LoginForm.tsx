3d187fd53f0d6896d3f57a5eae574f33755ec97f
gopalghosh-dev <gopalghosh.dev@gmail.com>
2026-01-16 19:10:45 +0530

    Add register form and enhance login form UI
    Implemented a complete RegisterForm component with validation and UI elements. Updated LoginForm to include a 'Remember Me' checkbox and improved the submit button with loading and icon states for better user experience.

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -2,7 +2,10 @@
 
 import { loginSchema, LoginSchemaType } from "@/lib/zodSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { FingerprintIcon, LoaderIcon } from "lucide-react";
 import { Controller, useForm } from "react-hook-form";
+import { Button } from "../shadcnui/button";
+import { Checkbox } from "../shadcnui/checkbox";
 import { Field, FieldError, FieldLabel } from "../shadcnui/field";
 import { Input } from "../shadcnui/input";
 
@@ -69,23 +72,42 @@ const LoginForm = () => {
 			/>
 
 			<Controller
-				name="password"
+				name="rememberMe"
 				control={control}
 				render={({ field, fieldState }) => (
-					<Field data-invalid={fieldState.invalid}>
-						<FieldLabel htmlFor={field.name}>Password</FieldLabel>
-						<Input
-							{...field}
+					<Field
+						data-invalid={fieldState.invalid}
+						orientation="horizontal">
+						<Checkbox
 							id={field.name}
-							aria-invalid={fieldState.invalid}
-							type="password"
-							placeholder="Enter Your Password"
-							autoComplete="current-password"
+							name={field.name}
+							checked={field.value}
+							onCheckedChange={field.onChange}
 						/>
+
+						<FieldLabel htmlFor={field.name}>Remember Me</FieldLabel>
+
 						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
 					</Field>
 				)}
 			/>
+
+			<Button
+				type="submit"
+				className="cursor-pointer"
+				disabled={isSubmitting}>
+				{isSubmitting ? (
+					<>
+						<LoaderIcon className="animate-spin" />
+						Submitting
+					</>
+				) : (
+					<>
+						<FingerprintIcon />
+						Login
+					</>
+				)}
+			</Button>
 		</form>
 	);
 };
--- src/components/Forms/RegisterForm.tsx
+++ src/components/Forms/RegisterForm.tsx
@@ -1,5 +1,134 @@
+"use client";
+
+import { registerSchema, RegisterSchemaType } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoaderIcon, UserPlusIcon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "../shadcnui/button";
+import { Field, FieldError, FieldLabel } from "../shadcnui/field";
+import { Input } from "../shadcnui/input";
+
 const RegisterForm = () => {
-	return <></>;
+	const {
+		handleSubmit,
+		control,
+		formState: { isSubmitting },
+	} = useForm({
+		resolver: zodResolver(registerSchema),
+		defaultValues: {
+			name: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+		},
+		mode: "all",
+	});
+
+	const registerFormHandeler = async (registerFormData: RegisterSchemaType) => {
+		await new Promise<void>((r) => setTimeout(r, 1000));
+
+		console.log(registerFormData);
+	};
+
+	return (
+		<form
+			onSubmit={handleSubmit(registerFormHandeler)}
+			className="grid gap-4"
+			noValidate>
+			<Controller
+				name="name"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Name</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							type="name"
+							placeholder="Enter Your Name"
+							autoComplete="name"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="email"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Email</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							type="email"
+							placeholder="Enter Your Email"
+							autoComplete="email"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="password"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Password</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							type="password"
+							placeholder="Your Password"
+							autoComplete="new-password"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Controller
+				name="confirmPassword"
+				control={control}
+				render={({ field, fieldState }) => (
+					<Field data-invalid={fieldState.invalid}>
+						<FieldLabel htmlFor={field.name}>Confirm Password</FieldLabel>
+						<Input
+							{...field}
+							id={field.name}
+							aria-invalid={fieldState.invalid}
+							type="password"
+							placeholder="Confirm Your Password"
+							autoComplete="current-password"
+						/>
+						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+					</Field>
+				)}
+			/>
+
+			<Button
+				type="submit"
+				className="cursor-pointer"
+				disabled={isSubmitting}>
+				{isSubmitting ? (
+					<>
+						<LoaderIcon className="animate-spin" />
+						Submitting
+					</>
+				) : (
+					<>
+						<UserPlusIcon />
+						Register
+					</>
+				)}
+			</Button>
+		</form>
+	);
 };
 
 export default RegisterForm;
